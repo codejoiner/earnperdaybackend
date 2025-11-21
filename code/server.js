afddb5085e6006a42a6b557e5754468bac3f3f26
codejoiner <codejoiner15@gmail.com>
2025-11-21 16:34:34 +0200

    ..........

--- code/server.js
+++ code/server.js
@@ -16,7 +16,7 @@ const conn = require('../conn/dbconn.js');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ;
 
 
 app.use(express.json());
