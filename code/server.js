833988bc55bdf2c99ed10c4f06eea8fe2576dc70
codejoiner <codejoiner15@gmail.com>
2025-11-21 16:55:50 +0200

    ............

--- code/server.js
+++ code/server.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['https://earnperday.vercel.app'],
     methods: ['GET', 'POST', 'DELETE', 'PATCH', 'PUT'],
     credentials: true
 }));
